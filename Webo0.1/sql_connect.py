9d01b8eb64f04657884882db4eac836602bc67d0
2439 <2439504029@qq.com>
2021-04-30 01:56:03 +0800

    2021/4/30 first

--- Webo0.1/sql_connect.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import mysql.connector
-import os
-
-conn = mysql.connector.connect(user="root", password="mysql", database="web")    # 数据库连接
-cursor = conn.cursor()
-
-app = Flask(__name__)     # 引入Flask
-app.config['SECRET_KEY'] = os.urandom(24)
